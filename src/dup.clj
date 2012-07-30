21fb7ebde747c22bee0fbc157499987d4d2cc2a4
waynebx <nguyenbx.tmwstw@gmail.com>
2012-07-30 19:24:54 +0700

    enable true parallel

--- src/dup.clj
+++ src/dup.clj
@@ -1,45 +1,52 @@
 (load "file_utils")
 (load "md5")
 
-(def ^:dynamic *dup-algo*)
-(def ^:dynamic *dup-agent*)
+(defn parse [id-fn filename]
+  (let [key (id-fn filename)]
+    {key [filename]}))
 
-(defn parse [dups filename id-fn]
-  (let [key (id-fn filename)
-        currents (dups key)]
-    (merge dups {key (conj currents filename)})))
+(def parse-by-md5
+  (partial parse md5sum))
 
-(defn parse-by-md5 [dups filename]
-  (parse dups filename md5sum))
+(def parse-by-name
+  (partial parse
+    (fn [f] (.getName (File. ^String f)))))
 
-(defn parse-by-name [dups filename]
-  (parse dups filename
-    (fn [^String f]
-      (.getName (File. f)))))
+; for examining effectiveness of threading
+(def parse-slowly
+  (partial parse
+    (fn [f] (Thread/sleep 10) f)))
 
-(defn find-dup [file-entries to-dos dup-agent dup-algorithm]
-  (let [todos (transient to-dos)]
-    (doseq [entry file-entries]
-      (if (is-file? entry)
-        (send dup-agent dup-algorithm entry)
-        (when (is-directory? entry)
-          (conj! todos entry))))
-    (persistent! todos)))
+(def ^:dynamic *id-algo*)
 
-(defn find-dups [start-dir dup-agent dup-algo]
-  (loop [todos [start-dir]]
-    (when-not (empty? todos)
-      (let [file-entries (list-of-files (first todos))]
-        (if (empty? file-entries)
-          (recur (-> todos rest vec))
-          (recur (-> (find-dup file-entries todos dup-agent dup-algo)
-                   rest vec)))))))
+(defn identify-and-merge
+  [all-files id-algo]
+  (let [agents (map agent all-files)]
+    (doseq [agent agents] (send-off agent id-algo))
+    (apply await agents)
+    (let [results (doall (pmap deref agents))]
+      (reduce (partial merge-with concat) results))))
 
-(defn go []
-  (binding [*dup-algo* parse-by-md5
-            *dup-agent* (agent {})]
+(defn find-dups [start-dir]
+  (let [todo-dirs (transient [start-dir])
+        todo-files (transient [])]
+    (loop []
+      (if-let [next-todo (nth todo-dirs (-> todo-dirs count dec) nil)]
+        (let [file-entries (list-of-files next-todo)]
+          (pop! todo-dirs)
+          (when-not (empty? file-entries)
+            (letfn [(conj-entries! [entries pred]
+                      (reduce conj! entries (filter pred file-entries)))]
+              (conj-entries! todo-dirs is-directory?)
+              (conj-entries! todo-files is-file?)))
+          (recur))
+        (identify-and-merge (persistent! todo-files) *id-algo*)))))
+
+(defn go [start-dir]
+  (binding [*id-algo* parse-by-name]
     (println "Starting...")
-    (find-dups "/home/gnt/tmp" *dup-agent* *dup-algo*)
-    (await *dup-agent*)
-    (let [dups (filter #(> (count (val %)) 1) @*dup-agent*)]
-      (println dups))))
+    (doseq [dup
+            (filter
+              #(> (count (val %)) 1)
+              (find-dups start-dir))]
+      (println dup))))
